9d7a257875eb34cac62007766348463969ac2bec
Whimsicalcodingman <94443201+Whimsicalcodingman@users.noreply.github.com>
2021-11-16 10:21:41 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 function display_product_category_descriptions(){
   if (category_description($category_id)) {
     echo category_description($category_id);
-} else {
+} 
+// This is a fallback for whenever the shop page shows all products and needs to show a custom message as well. If left empty it will show nothing if on standard shop page
+else {
 	  echo '<div>Ontdek alle natuurlijke producten van Marie-Stella-Maris. Van zachte lichaamsverzorging tot luxueuze huisparfums. Ontworpen met aandacht voor detail. In een verscheidenheid van originele unisex geuren. Voor elk product dat je koopt, doneren we een vast bedrag aan schoondrinkwater-projecten wereldwijd.</div>';
   }
 }
